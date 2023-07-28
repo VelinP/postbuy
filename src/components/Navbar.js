0d7ba0468910e20ec82b69271c27aedeba68607a
VelinP <dottallot1@hotmail.com>
2023-07-28 13:32:12 +0300

    Made navbar and started working on project

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+import {FaHome} from 'react-icons/fa'
+
+export const Navbar = ()=>{
+
+    return(
+
+    <nav className='mainnav'>
+        <ul className='mainul'>
+            <>
+
+            <Link to ="/"><FaHome size={'1.4rem'}/></Link>
+            <Link to ="/bags">Bags</Link>
+            <Link to ="/shoes">Shoes</Link>
+            <Link to ="/watches">Watches</Link>
+            <Link to ="/mens">Mens</Link>
+            <Link to ="/womens">Womens</Link>
+
+
+
+            
+
+            </>
+        
+        
+
+        </ul>
+    </nav>
+
+    )
+}
