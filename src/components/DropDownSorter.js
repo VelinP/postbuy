79d9f89076e6c95d8c0cde7b84f1fb364e0764a9
VelinP <dottallot1@hotmail.com>
2023-08-09 15:33:01 +0300

    cleaned up code(added callbacks)

--- src/components/DropDownSorter.js
+++ src/components/DropDownSorter.js
@@ -1,58 +1,14 @@
 
-import { useEffect } from "react";
-
-
-
 export const DropDownSorter = (props) =>{
-    
-    useEffect(()=> {
-
-        const closedropdown = (e) =>{
-            console.log(e);
-            
-        };
-
-        document.body.addEventListener('click', closedropdown);
-        return () => document.body.removeEventListener('click', closedropdown)
-
-
-    },[]
-    )
-
-
-    const sortpricefunchighest = () =>{
-
-        props.info.sort((p1, p2) => (p2.price - p1.price));
-        props.state(false)
-    }
-
-    const sortpricefunlowest = () =>{
-        props.info.sort((p1, p2) => (p1.price - p2.price));
-        props.state(false)
-    }
-
-    const sortalhabeticallyascending = () =>{
-        props.info.sort((a, b) => a.brand.localeCompare(b.brand))
-        props.state(false)
-
-        
-    }
-
-    const sortalhabeticallydescending = () =>{
-        props.info.sort((a, b) => b.brand.localeCompare(a.brand))
-        props.state(false)
-
-        
-    }
     
     return(
         <div className="maindropdownmenu" >
         <div className={'dropdownmenu'}>
         
-                <button onClick={sortpricefunchighest}>Most expensive</button>
-                <button onClick={sortpricefunlowest}>Least Expensive</button>
-                <button onClick={sortalhabeticallyascending}>Ascending Alphabetically</button>
-                <button onClick={sortalhabeticallydescending}>Descending Alphabetically</button>
+                <button onClick={()=>{props.info.sort((p1, p2) => (p2.price - p1.price));props.state(false)}}>Most expensive</button>
+                <button onClick={()=>{props.info.sort((p1, p2) => (p1.price - p2.price));props.state(false)}}>Least Expensive</button>
+                <button onClick={()=>{props.info.sort((a, b) => a.brand.localeCompare(b.brand));props.state(false)}}>Ascending Alphabetically</button>
+                <button onClick={()=>{props.info.sort((a, b) => b.brand.localeCompare(a.brand));props.state(false)}}>Descending Alphabetically</button>
 
         </div>
 
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -7,113 +7,30 @@ export const Filter = (props) =>{
     //  i had some issues with filter because it changed the default state so if i filtered it 2-3 times it just didnt show any items after filtering
     //  but with the static state since it never changes i got it to work
     const brands = ['Valentino Garavani', 'Prada', 'Balenciaga', 'Rado', 'Tissot', 'Hugo Boss', 'Gucci']
-
-    const discountfilter = () =>{
-        props.state(props.static.filter(item => item.discount !== ""))
-        
-    }
-    
-    const shopbypricefilter500 = () =>{
-        props.state(props.static.filter(item => item.price >= 100 && item.price <=500))
-    }
-
-    const shopbypricefilter2000 = () =>{
-        props.state(props.static.filter(item => item.price >= 500 && item.price <=2000))
-
-    }
-
-    const shopbypricefilter5000 = () =>{
-        props.state(props.static.filter(item => item.price >= 2000 && item.price <=5000))
-
-    }
-
-    const shopbypricefilterabove5000 = () =>{
-        props.state(props.static.filter(item => item.price >= 5000))
-
-    }
-
-    const allproductsfilter = () =>{
-        props.state(props.static)
-    }
-    
-    const allproductsfiltergucci = () =>{
-        props.state(props.static.filter(item => item.brand === 'Gucci'))
-
-    }
-
-    const shopbypricefilterGaravani = () =>{
-        props.state(props.static.filter(item => item.brand === 'Valentino Garavani'))
-
-    }
-
-    const shopbypricefilterPrada = () =>{
-        props.state(props.static.filter(item => item.brand === 'Prada'))
-
-    }
-
-    const shopbypricefilterBalenciaga =() =>{
-        props.state(props.static.filter(item => item.brand === 'Balenciaga'))
-
-    }
-
- 
- 
-
-    const shopbypricefilteraboveRado = () =>{
-        props.state(props.static.filter(item => item.brand === 'Rado'))
-
-    }
-
-    const shopbypricefilteraboveTissot = () =>{
-        props.state(props.static.filter(item => item.brand === 'Tissot'))
-
-    }
-
-    const shopbypricefilteraboveHugo = () =>{
-        props.state(props.static.filter(item => item.brand === 'Hugo Boss'))
-
-    }
-
-    const shopbypricefilteraboveother = () =>{
-        props.state(props.static.filter(item => !brands.includes(item.brand)))
-        //checks the array of brands at the top and filters in all the items not in it
-    }
-
-    const shopbypricefilterless500 = () =>{
-        props.state(props.static.filter(item => item.price < 100))
-        
-    }
-
-
     return(
 
         <div className="filterdiv">
             <h1 >Filter</h1>
-            <div onClick={discountfilter}>Discount</div>
-            <div onClick={allproductsfilter}>Show All Products</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.discount !== ""))}}>Discount</div>
+                <div onClick={()=>{props.state(props.static)}}>Show All Products</div>
 
             <h1>Filter Price</h1>
-            <div onClick={shopbypricefilterless500}>Less than $100</div>
-            <div onClick={shopbypricefilter500}>$100 - $500</div>
-            <div onClick={shopbypricefilter2000}>$500 - $2000</div>
-            <div onClick={shopbypricefilter5000}>$2000 - $5000</div>
-            <div onClick={shopbypricefilterabove5000}>Over $5000</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.price < 100))}}>Less than $100</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.price >= 100 && item.price <=500))}}>$100 - $500</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.price >= 500 && item.price <=2000))}}>$500 - $2000</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.price >= 2000 && item.price <=5000))}}>$2000 - $5000</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.price >= 5000))}}>Over $5000</div>
             <h1>Filter by Brands</h1>
 
-            <div onClick={allproductsfiltergucci}>Gucci</div>
-            <div onClick={shopbypricefilterGaravani}>Valentino Garavani</div>
-            <div onClick={shopbypricefilterPrada}>Prada</div>
-            <div onClick={shopbypricefilterBalenciaga}>Balenciaga</div>
-        
-            <div onClick={shopbypricefilteraboveRado}>Rado</div>
-            <div onClick={shopbypricefilteraboveTissot}>Tissot</div>
-            <div onClick={shopbypricefilteraboveHugo}>Hugo Boss</div>
-            <div onClick={shopbypricefilteraboveother}>Other</div>
-
-
-
-
-
+                <div onClick={()=>{props.state(props.static.filter(item => item.brand === 'Gucci'))}}>Gucci</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.brand === 'Valentino Garavani'))}}>Valentino Garavani</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.brand === 'Prada'))}}>Prada</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.brand === 'Balenciaga'))}}>Balenciaga</div>
+            
+                <div onClick={()=>{props.state(props.static.filter(item => item.brand === 'Rado'))}}>Rado</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.brand === 'Tissot'))}}>Tissot</div>
+                <div onClick={()=>{props.state(props.static.filter(item => item.brand === 'Hugo Boss'))}}>Hugo Boss</div>
+                <div onClick={()=>{props.state(props.static.filter(item => !brands.includes(item.brand)))}}>Other</div>
 
         </div>
 
